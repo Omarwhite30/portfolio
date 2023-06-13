5902a9d282da1174a0c90798e5e0bb2ee2325c79
Omarwhite30 <omarwhite2@aol.com>
2023-06-13 19:07:33 -0400

    updated colors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -57,22 +57,22 @@ const Home = () => {
   };
 
   return (
-    <div style={{ backgroundColor: '#ADD8E6', minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-      <h1 style={{ textAlign: 'center', color: '#FFFFFF',  marginRight: '100rem', fontSize: '33px'}}>Omar A. White</h1>
-      <div style={{ backgroundColor: '#FFFFFF', borderRadius: '40px', padding: '1rem', margin: '40rem', textAlign: 'center', marginRight:'55rem', marginTop: '3rem'}}>
-        <p>
+    <div style={{ backgroundColor: 'black', minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+      <h1 style={{ textAlign: 'center', color: '#FFFFFF',  marginRight: '75rem', fontSize: '33px', fontStyle:'comic sans'}}>Omar A. White</h1>
+      <div style={{ backgroundColor: 'green', borderRadius: '40px', padding: '1rem', margin: '16rem', textAlign: 'center', marginRight:'35rem', marginTop: '1rem'}}>
+        <p style={{ fontFamily: 'Comic Sans MS' }}>
           Hello and welcome to my portfolio. I am a Software Developer and military veteran. Here is where you'll be able to stay up to date with me. I have links to projects and contact info for you to get in touch with me. Have a look around, and please don't be afraid to reach out. Enjoy your day!
         </p>
       </div>
       {/* Add your profile picture and contact information */}
-      <footer style={{ marginLeft: '100rem' }}>
-        <a href="https://github.com/omarwhite30" target="_blank" rel="noreferrer" style={{ color: '#000000', marginRight: '1rem' }}>
+      <footer style={{ marginLeft: '80rem' }}>
+        <a href="https://github.com/omarwhite30" target="_blank" rel="noreferrer" style={{ color: 'white', marginRight: '1rem' }}>
           <FaGithub size={32} />
         </a>
-        <a href="https://www.linkedin.com/in/omarawhite/" target="_blank" rel="noreferrer" style={{ color: '#000000', marginRight: '1rem' }}>
+        <a href="https://www.linkedin.com/in/omarawhite/" target="_blank" rel="noreferrer" style={{ color: 'white', marginRight: '1rem' }}>
           <FaLinkedin size={32} />
         </a>
-        <button onClick={handleEmailButtonClick} style={{ color: '#000000', border: 'none', background: 'none', cursor: 'pointer' }}>
+        <button onClick={handleEmailButtonClick} style={{ color: 'white', border: 'none', background: 'none', cursor: 'pointer' }}>
           <FaEnvelope size={32} />
         </button>
       </footer>
