4f838796073891528a13561fdd16197d42d12d13
Omarwhite30 <omarwhite2@aol.com>
2023-06-27 19:18:57 -0400

    updates

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,15 @@
+import { Router } from "react-router-dom";
+import {Link} from "react-router-dom"
+
+
 const Navbar = () =>{
     return(
         <nav className= "navbar">
             <h1>Omar's Portfolio</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/">Projects</a>
-                <a href="/">Reflection</a>
+                <Link to="/">Home</Link>
+                <Link to="/">Projects</Link>
+                <Link to="/">Reflection</Link>
             </div>
 
         </nav>
